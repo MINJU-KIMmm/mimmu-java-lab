6e0957547375fee9292ed5efcab2bbfae5a20e58
MINJU-KIMmm <0112km@ewhain.net>
2022-11-26 22:22:24 +0900

    remove : 필요 없는 파일 삭제

--- mimmu-chat/src/main/java/com/mimmu/mimmuchat/repository/RoomUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mimmu.mimmuchat.repository;
-
-import com.mimmu.mimmuchat.Entity.RoomUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoomUserRepository extends JpaRepository<RoomUser, Long> {
-}
