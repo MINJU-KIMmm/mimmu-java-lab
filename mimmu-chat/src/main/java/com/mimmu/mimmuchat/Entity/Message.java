230f75509c9bf32692793965b1884cc3a86b371b
MINJU-KIMmm <0112km@ewhain.net>
2022-11-26 22:29:11 +0900

    feat : 메시지 도메인 추가

---
+++ mimmu-chat/src/main/java/com/mimmu/mimmuchat/Entity/Message.java
@@ -0,0 +1,54 @@
+package com.mimmu.mimmuchat.Entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Table(name = "message")
+@Entity
+public class Message{
+
+    @Id
+    @Column(name = "message_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "chat_room_id", nullable = false)
+    private ChatRoom chatRoom;
+
+    @ManyToOne
+    @JoinColumn(name = "sender")
+    private ChatUser sender;
+
+//    @ManyToOne
+//    @JoinColumn(name = "receiver")
+//    private ChatUser receiver;
+//
+    @Column(nullable = false)
+    private String message;
+
+    @Column(nullable = false)
+    private LocalDateTime sendtime;
+
+    @PrePersist
+    public void createDate(){
+        this.sendtime = LocalDateTime.now();
+    }
+
+
+
+    @Builder
+    public Message(Long id, ChatRoom chatRoom, ChatUser sender, String message) {
+        this.id = id;
+        this.chatRoom = chatRoom;
+        this.sender = sender;
+        this.message = message;
+    }
+
+}
