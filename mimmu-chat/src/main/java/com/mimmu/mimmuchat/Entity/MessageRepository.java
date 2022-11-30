3300cc28cbeece1b4b7020c032910b89b3979f07
MINJU-KIMmm <0112km@ewhain.net>
2022-11-30 17:32:21 +0900

    fix : 방과 유저 입장시간으로만 찾기

--- mimmu-chat/src/main/java/com/mimmu/mimmuchat/Entity/MessageRepository.java
+++ mimmu-chat/src/main/java/com/mimmu/mimmuchat/Entity/MessageRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
-    public List<Message> findAllByChatRoomAndSenderAndSendtimeIsAfter(ChatRoom chatRoom, ChatUser sender, LocalDateTime enterTime);
+    public List<Message> findAllByChatRoomAndSendtimeIsAfter(ChatRoom chatRoom, LocalDateTime enterTime);
 }
